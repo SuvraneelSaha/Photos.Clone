2b9042106f7af1e83c24f3968e9588f96b3f229d
Suvraneel _Saha <suvroneel2001@gmail.com>
2025-02-05 13:03:40 +0530

    GetMapping for photo all and also for photo with specific id

--- src/main/java/com/Aggin/Codes/Photos/Clone/PhotoController.java
+++ src/main/java/com/Aggin/Codes/Photos/Clone/PhotoController.java
@@ -1,19 +1,35 @@
 package com.Aggin.Codes.Photos.Clone;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class PhotoController {
 
-    private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
+    private Map<String,Photo> db = new HashMap<>(){{
+        put("1",new Photo("1", "hello.jpg"));
+    }};
 
-    @GetMapping("/photoz")
-    public List<Photo> getPhoto(){
-        return db;
+
+    // this method returns all the photos in the db
+    @GetMapping("/photoz/")
+    public Collection<Photo> getPhoto(){
+        return db.values();
+        // values is a method of the Collection Framework
     }
+
+    // this method will return only the specific id
+    @GetMapping("/photoz/{id}")
+    public Photo getPhoto(@PathVariable String id){
+        Photo photo =  db.get(id);
+        if(photo==null) throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Photo with the specific Id is not found");
+        return photo;
+    }
+
 
 }
